81323987473b620c53ae21e9893892fde411fdde
SREEDHIL <sreedhilpavishanker@gmail.com>
2025-02-20 11:59:11 +0530

    updated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
-// src/components/Home.jsx
-
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import BookCard from "./components/BookCard";
 import books from "./booksData";
 import "./Home.css"; // Import the CSS for styling
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="home-container">
       <h1>Book Library</h1>
@@ -14,6 +15,7 @@ const Home = () => {
           <BookCard key={book.id} book={book} />
         ))}
       </div>
+      <button onClick={() => navigate("/add-book")}>Add Book</button>
     </div>
   );
 };
